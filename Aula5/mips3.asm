20b50cedd5b1d103c2f749be8854a56b9ee8085d
Beatriz Nascimento <131934165+beatrizgnascimento@users.noreply.github.com>
2024-06-13 20:59:18 -0300

    aaaaa

--- Aula5/mips3.asm
+++ Aula5/mips3.asm
@@ -5,35 +5,47 @@ impar:	.asciiz "impar"
 .globl main
 # Recebe o valor em a0
 # Devolve em v0: 1(sim) 0 (nao)
+
+#Isolar o último bit do número (se for 0 é par, se for 1 é ímpar)
 main:
 	#leitura do valor
 	li $v0, 5 #read int
-	syscall
+	syscall #o valor digitado está em $v0
 	
-	#parametro em $a0
-	add $a0, $zero, $v0
+	#passar o argumento n para $a0
+	add $a0, $zero, $v0 #$a0 = $v0
 	
-	jal quad
+	jal verifica #jump and link
+	
+	# a resposta está em v0
 	
 	add $s0, $zero, $v0
-if2:
-	bne $s0, $zero, else2
-then2:
+if:
+	#if v0 = 0 ->par
+	bne $s0, $zero, else
+then:
 	li $v0, 4
 	la $a0, par
 	syscall
-else2:
+	j endIf
+	
+else:	# ímpar
 	li $v0, 4
 	la $a0, impar
 	syscall
 
-endIf2:
+endIf:
 	#encerra
 	li $v0, 10
 	syscall
 
 .data
 .text
-quad:
-	andi $t0, $a0, 1
+#Se for par, retorna 0 em $v0
+#Se for ímpar, retorna 1 em $v0
+verifica:
+	# $a0 = 101011
+	#	000001
+	# $v0 =	000001
+	andi $t0, $a0, 1 #A operação andi é bit a bit e faz o and entre as unidades (multiplicação)
 	jr $ra
---
+++ Aula6/mips1.asm
@@ -0,0 +1,66 @@
+# Escrever um subprograma que verifique se um valor informado pelo usuário é um número primo
+
+.data
+primo:		.asciiz "primo"
+nprimo:		.asciiz "composto"
+
+.text
+.globl main
+main:
+	li $v0, 5 #read int
+	syscall #o valor digitado está em $v0
+	
+	#passar o argumento n para $a0
+	add $a0, $zero, $v0 #$a0 = $v0
+	
+	jal verifica #jump and link
+	
+if3:
+	bne $v0, $zero, else3 #se ele não é 0, é primo
+then3:	
+	la $a0, nprimo
+	li $v0, 4
+	syscall
+	j endIf3
+else3:
+	la $a0, primo
+	li $v0, 4
+	syscall
+	j endIf3
+endIf3:
+	li $v0, 10
+	syscall
+
+.data
+.text
+verifica:
+	#Recebe o numero em a0
+	# $a0 = n
+	li	$t0, 0 #cont = 0
+	li	$t1, 1 #i = 1
+	
+	addi $t2, $a0, 1 # t2 = (n + 1)
+	
+inicio:
+	#so vai escapar do loop quando i = n + 1
+	beq $11, $t2, fim
+	
+	# verifica o resto da divisão
+	rem $t3, $a0, $t1 # t3 = n % i
+	
+if:
+	bne $t3, $zero, endIf 
+	addi $t0, $t0, 1
+endIf:
+	#incrementar o i
+	addi	$t1, $t1, 1 # i = i + 1
+	j	inicio
+	
+fim:
+	li $t4, 2 
+	li $v0, 0 #salva a resposta, nao primo
+if2:
+	beq $t0, $t4, endIf2
+	li $v0, 1 #salva a respotsa em v0, primo
+endIf2:
+	jr $ra

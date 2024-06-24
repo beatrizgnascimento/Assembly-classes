f97a0d319f82c385cfdaa3fed98296e6a4db8f5f
Beatriz Nascimento <131934165+beatrizgnascimento@users.noreply.github.com>
2024-06-24 09:56:03 -0300

    corrigindo o ex do array
    voce esqueceu de incrementar o contador para a próxima posição do array na leitura e esqueceu de colocar isso no maior_valor. Lembre-se de sempre incrementar o endereço do array MAIS incrementar o contador para a próxima posição !@!!

---
+++ Lista3/exJ.asm
@@ -0,0 +1,46 @@
+.data
+v:	.space 24 # array de 6 espaços
+.text
+.globl main
+main:
+	jal leitura
+	jal maior_valor
+	
+	move $a0, $v1 #imprimir o maior valor
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+
+
+leitura:
+	la $t1, v #carrega o endereço do array no registrador t1
+	li $t0, 0 #contador da posição do array
+while1:
+	li $v0, 5 #Ler inteiro
+	syscall
+	sw $v0, 0($t1) #sw = armazenar o valor de um registrador em uma posição de memória 
+	#armazenar o valor de v0 no array
+	addi $t1, $t1, 4  # incrementar o endereço do array para a próxima posição
+	addi $t0, $t0, 1 # incrementar contador para a próxima posição do array
+	blt $t0, 6, while1 #loop vai até 6 valores
+	jr $ra
+
+
+maior_valor:
+	la $t1, v #carrega o endereço do array no registrador t1
+	li $t0, 0 #cont = 0
+	lw $t2, 0($t1) # Carrega o primeiro valor do array
+	move $t3, $t2 # maior = v[0].
+while2:
+	lw $t2, 0($t1) # ler elemento atual
+	bgt $t2, $t3, then1 #Se atual > maior, atualizar o valor
+	addi $t1, $t1, 4 # incrementar endereço do array para a próxima posição
+	addi $t0, $t0, 1 # incrementar contador
+	blt $t0, 6, while2 #loop percorre todo o array
+	move $v1, $t3 # retorna o maior valor
+	jr $ra
+then1:
+	move $t3, $t2 # atualizar o valor de t3 (maior valor)
+	j while2
---
+++ taking_notes.asm
@@ -0,0 +1,24 @@
+##--------------------Registradores-----------------
+# $v0 - registrador de valor de retorno. Quando um subprograma retorna um valor, ele é armazenado em v0
+# a0 - registrador de argumento. Quando voce quer imprimir um valor usando a syscall com código 1(imprimir inteiro), voce
+# precisa colocar o valor em a0
+
+#--------------------Array------------------------
+#	Para criar array com espaços definidos
+#Ex: 6 espaços = 24. Espaõs x 4 = Número do .data
+#.data
+#v:	.space 24
+
+#	Carregar o endereço do array em um registrador
+#la $t1, v #carrega o endereço do array no registrador t1
+
+#	Criar um contador para percorrer o array
+#li $t0, 0 #contador da posição do array 
+	
+#	Armazenar o valor em uma posição do array
+#sw $v0, 0($t1) #sw = armazenar o valor de um registrador em uma posição de memória 
+#armazenar o valor de v0 no array
+
+#	Para percorrer o array (endereço + posição)
+# addi $t1, $t1, 4  # incrementar o endereço do array para a próxima posição
+# addi $t0, $t0, 1 # incrementar contador para a próxima posição do array
